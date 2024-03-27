133c1a5d5104962a8431e19b5f1937f0f9e61c1c
almazheyderli <bd7g7vr18@code.edu.az>
2024-03-28 03:09:45 +0400

    getset

---
+++ almazgetset/almazgetset/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace almazgetset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
